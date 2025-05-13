2bdffac1f85ce3c2772359dd720a709a271e4465
VigneshM454 <vigneshcse454@gmail.com>
2025-05-14 01:04:56 +0530

    included netlify url

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(cors({
       'http://localhost:4200', 'http://127.0.0.1:4200',  
       'http://localhost:5000', 'http://127.0.0.1:5000',
       'http://localhost:5173', 'http://127.0.0.1:5173',
-      'https://ecganalyser.netlify.app/'
+      'https://ecganalyser.netlify.app'
     ]
 }))
 app.use(cookieParser())
